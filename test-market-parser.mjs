b91cc9ccfff9f44b64145ec47b9be2b37828012c
bullstarsignals-lab <bullstarsignals@gmail.com>
2026-09-02 02:50:14 +0200

    Update test-market-parser.mjs

--- test-market-parser.mjs
+++ test-market-parser.mjs
@@ -25,7 +25,7 @@ const SAMPLE_HTML = `
   <div class="tgme_widget_message photo_belowtext_wrap js-widget_message" data-post="BULLSTAR1/5531" data-view="def456">
     <div class="tgme_widget_message_bubble">
       <a class="tgme_widget_message_photo_wrap" style="background-image:url('https://cdn4.telesco.pe/file/sample123.jpg')" href="https://t.me/BULLSTAR1/5531"></a>
-      <div class="tgme_widget_message_text js-message_text" dir="auto">ETH looking strong above $3,400 - watching for continuation.</div>
+      <div class="tgme_widget_message_text js-message_text" dir="auto">ETH looking strong above &#036;3,400 - watching for continuation.</div>
       <div class="tgme_widget_message_footer compact_message">
         <a class="tgme_widget_message_date" href="https://t.me/BULLSTAR1/5531">
           <time class="time" datetime="2026-08-30T09:02:00+00:00">09:02</time>
@@ -65,6 +65,7 @@ assert.equal(first.publishedAt, '2026-08-30T10:15:00.000Z');
 
 assert.equal(second.postId, 'BULLSTAR1/5531');
 assert.equal(second.image, 'https://cdn4.telesco.pe/file/sample123.jpg', 'photo url extracted');
+assert.ok(second.text.includes('above $3,400'), 'numeric entity &#036; decoded to $ (Telegram encodes some plain chars this way)');
 assert.deepEqual(second.tags, ['ETH']);
 
 assert.deepEqual(detectTags('nothing about coins here'), []);
